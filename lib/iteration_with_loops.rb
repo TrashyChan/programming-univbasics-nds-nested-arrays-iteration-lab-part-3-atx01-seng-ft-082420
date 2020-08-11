91e39ca2319c7f112a6407e5e5349e0a4ff48f8a
John Lonetree <jlonetree@gmail.com>
2020-08-11 18:13:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,16 @@
 def join_nested_strings(src)
   row_index = 0 
-  sentence = []
+  final_sentance = ""
   
   while row_index < src.count do 
     element_index = 0 
     while element_index < src[row_index].count do
       if src[row_index][element_index].is_a?(String)
-        words = src[row_index][element_index]
+        final_sentance =+ src[row_index][element_index] + " "
     end
-      sentence << words
       element_index += 1
     end
     row_index += 1
   end
-  sentence.uniq.join(" ")
+  final_sentence.uniq.join(" ")
 end
